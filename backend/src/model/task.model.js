1246c1f61312aec7c64106aa0f0145f18507ee43
nouri50 <n.morouche@gmail.com>
2024-09-15 21:52:37 +0200

    correction de la base de donnee

--- backend/src/model/task.model.js
+++ backend/src/model/task.model.js
@@ -1,25 +1,20 @@
 import { DataTypes } from 'sequelize';
-import sequelize from '../config/db.config.js'; // Assurez-vous que le chemin est correct
+import sequelize from '../config/db.config.js'; // Chemin correct vers db.config.js
 
-// Définition du modèle Task
 const Task = sequelize.define('Task', {
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  status: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    defaultValue: 'pending',
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: 'pending',
+    }
 });
 
 export default Task;
