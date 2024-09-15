9be375215945240fadc60173edc0cdd20be8de0c
nouri50 <n.morouche@gmail.com>
2024-09-15 21:52:37 +0200

    correction de la base de donnee

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -1,17 +1,18 @@
 import { Sequelize } from 'sequelize';
 import 'dotenv/config';
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  logging: false,
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connexion à la base de données réussie.');
+  })
+  .catch(err => {
+    console.error('Impossible de se connecter à la base de données:', err);
+  });
 
 export default sequelize;
-
-  
