4956aa52d6a498bf7127caceb163fddac02b4e1d
Nesrine Zitouni <nesrinezitouni@MacBook-Air-de-Nesrine.local>
2024-09-16 00:34:41 +0200

    modification dossier backend

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -1,26 +1,22 @@
+// Importer Sequelize depuis le package sequelize
 import { Sequelize } from 'sequelize';
-import 'dotenv/config';
+import 'dotenv/config';  // Charger les variables d'environnement depuis le fichier .env
 
-// Créer une nouvelle instance Sequelize avec les informations de connexion
+// Créer une instance de Sequelize
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
-  logging: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
+  port: process.env.DB_PORT || 3306,  // Si le port est différent, utilisez celui dans .env
+  logging: false,  // Désactiver les logs SQL
 });
 
-// Vérifier la connexion à la base de données
+// Tester la connexion à la base de données
 sequelize.authenticate()
   .then(() => {
     console.log('Connexion à la base de données réussie.');
   })
-  .catch((err) => {
-    console.error('Impossible de se connecter à la base de données:', err);
+  .catch(err => {
+    console.error('Impossible de se connecter à la base de données :', err);
   });
 
 export default sequelize;
