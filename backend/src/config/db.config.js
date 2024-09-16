cf23ae5e75088b36f682a7dc90c7a628e5d9f32f
nouri50 <n.morouche@gmail.com>
2024-09-16 17:45:08 +0200

    correction due merge

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -1,36 +1,20 @@
-// Importer Sequelize depuis le package sequelize
 import { Sequelize } from 'sequelize';
 import 'dotenv/config';  // Charger les variables d'environnement depuis le fichier .env
 
-<<<<<<< HEAD
+// Créer une instance de Sequelize avec les informations de connexion de .env
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
-  logging: false,
-});
-
-=======
-// Créer une instance de Sequelize
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-  port: process.env.DB_PORT || 3306,  // Si le port est différent, utilisez celui dans .env
+  port: process.env.DB_PORT || 3306,  // Utilisez le port défini dans .env ou 3306 par défaut
   logging: false,  // Désactiver les logs SQL
 });
 
 // Tester la connexion à la base de données
->>>>>>> main
 sequelize.authenticate()
   .then(() => {
     console.log('Connexion à la base de données réussie.');
   })
   .catch(err => {
-<<<<<<< HEAD
-    console.error('Impossible de se connecter à la base de données:', err);
-  });
-
-export default sequelize;
-=======
     console.error('Impossible de se connecter à la base de données :', err);
   });
 
@@ -38,4 +22,4 @@ export default sequelize;
 
 
   
->>>>>>> main
+
