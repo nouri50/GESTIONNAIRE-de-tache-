dee63b9a7358ea03a0de9e23b77a8c07478e917d
Nesrine Zitouni <nesrinezitouni@MacBook-Air-de-Nesrine.local>
2024-09-28 13:14:53 +0200

    test automatisé modif password long

---
+++ tests/modif_password/modif_password_long/cypress/support/step_definitions/ModifPasswordLongStepDefinitions.spec.cy.js
@@ -0,0 +1,36 @@
+import { Given, When, Then, And } from '@badeball/cypress-cucumber-preprocessor';
+require('cypress-xpath')
+
+Given('I m on the website', () => {
+    cy.visit('http://localhost:3000')
+})
+
+Given('I connect on the website', () => {
+    cy.xpath('//a[normalize-space()="Connexion"]').click()
+    cy.xpath('//input[@placeholder="Email"]').type('nzito055@icloud.com')
+    cy.xpath('//input[@placeholder="Mot de passe"]').type('nZit11031994@')
+    cy.xpath('//button[normalize-space()="Connexion"]').click()
+})
+
+When('I redirected on profil page', () => {
+    cy.xpath('//a[normalize-space()="Profil"]').click()
+})
+
+When('I click on button edit password', () => {
+    cy.xpath('//button[normalize-space()="Modifier le mot de passe"]', { timeout : 10000 }).click() 
+})
+
+When('I edit my password', () => {
+    cy.xpath('//div[@class="change-password-page"]').type('nZit11031994@')
+    cy.xpath('//input[@type="password"]').type('jxhsdugzqydxgzyegdfyzegyhusashazudhuazdhyzegfygzebfzyegfyebgfyegfycegyfuo@')
+    cy.xpath('//*[@id="root"]').type('jxhsdugzqydxgzyegdfyzegyhusashazudhuazdhyzegfygzebfzyegfyebgfyegfycegyfuo@')
+})
+
+When('I confirm my new password', () => {
+    cy.xpath('//button[normalize-space()="Changer le mot de passe"]').click()
+})
+
+Then('I receive an error message', () => {
+    cy.xpath('//p[@class="message"]').should('be.visible')
+})
+
