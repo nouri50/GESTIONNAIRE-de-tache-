7d6673b4bb3612745b92315b18efbbbf4ae9f173
Nesrine Zitouni <nesrinezitouni@MacBook-Air-de-Nesrine.local>
2024-09-29 01:31:25 +0200

    test automatisé modif password court

---
+++ tests/modif_password/modif_password_court/cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin; // permet de lire le fichier en Gherkin
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin; // permet de faire la compilation des fichiers Gherkin
+
+module.exports = defineConfig({
+  e2e: {
+    async setupNodeEvents(on, config) {
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+
+      on("file:preprocessor", bundler);
+      await addCucumberPreprocessorPlugin(on, config);
+
+      return config;
+    },
+
+    defaultCommandTimeout: 90000,
+    specPattern: "cypress/e2e/features/*.feature"
+  },
+});
---
+++ tests/modif_password/modif_password_court/cypress/support/step_definitions/ModifPasswordCourtStepDefinitions.spec.cy.js
@@ -0,0 +1,33 @@
+import { Given, When, Then, And } from '@badeball/cypress-cucumber-preprocessor';
+require('cypress-xpath')
+
+Given('I m on the website', () => {
+    cy.visit('http://localhost:3000')
+})
+
+Given('I connect on the website', () => {
+    cy.xpath('//a[@href="/login"]').click()
+    cy.xpath('//input[@type="email"]').type('nzito055@icloud.com')
+    cy.xpath('//input[@type="password"]').type('nZit11031994@')
+    cy.xpath('//button[@type="submit"]').click()
+})
+
+When('I redirected on the edit password page', () => {
+    cy.get(':nth-child(4) > a').click() 
+    cy.xpath('//button[normalize-space()="Modifier le mot de passe"]', { timeout: 50000 }).click()  
+    cy.wait(9000) 
+})
+
+When('I fill the fields', () => {
+    cy.xpath('//input[@data-cy="current-password"]').type('nZit11031994@')
+    cy.xpath('//input[@data-cy="new-password"]').type('test')
+    cy.xpath('//input[@data-cy="confirm-password"]').type('test')
+})
+
+When('I confirm my new password', () => {
+    cy.xpath('//button[normalize-space()="Changer le mot de passe"]').click()
+})
+ 
+Then('I receive an error message', () => {
+    cy.xpath('//p[@class="message"]').should('be.visible')
+})
