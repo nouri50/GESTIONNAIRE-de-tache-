fbe51d5d2492901188aa0f182871a4f9c3c68ec1
Nesrine Zitouni <nesrinezitouni@MacBook-Air-de-Nesrine.local>
2024-09-29 17:34:20 +0200

    test automatisé modif password lié

---
+++ tests/modif_password/modif_password_lié/cypress/support/step_definitions/ModifPasswordLiéStepDefinitions.spec.cy.js
@@ -0,0 +1,34 @@
+import { Given, When, Then, And } from '@badeball/cypress-cucumber-preprocessor';
+require('cypress-xpath')
+
+Given('I am on the website', () => {
+    cy.visit('http://localhost:3000')
+})
+
+Given('I connect on the website', () => {
+    cy.xpath('//a[@href="/login"]').click()
+    cy.xpath('//input[@type="email"]').type('nzito055@icloud.com')
+    cy.xpath('//input[@type="password"]').type('testTest011@')
+    cy.xpath('//button[@type="submit"]').click()
+})
+
+When('I redirected on the edit password page', () => {
+    cy.wait(9000)
+    cy.xpath('//a[@href="/profil"]').click()
+    cy.get('[data-cy="profile-change-password-button"]').click()
+})
+
+When('I fill the fields', () => {
+    cy.xpath('//input[@data-testid="current-password-input"]').type('testTest011@')
+    cy.xpath('//input[@data-testid="new-password-input"]').type('testTest011@')
+    cy.xpath('//input[@data-testid="confirm-password-input"]').type('testTest011@')
+})
+
+When('I validate my new password', () => {
+    cy.wait(9000)
+    cy.get('[data-testid="submit-button"]').click()
+})
+
+Then('I receive an error message', () => {
+    cy.xpath('//p[@data-testid="message"]').should('be.visible')
+})
