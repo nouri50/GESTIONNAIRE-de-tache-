9153cff00b788e484d33e00e362224df19cbcfff
Nesrine Zitouni <nesrinezitouni@MacBook-Air-de-Nesrine.local>
2024-10-03 00:14:26 +0200

    test automatisé inscription utilisateur fonctionnelle

--- test/inscription_users/inscription_users_fonctionnal/cypress.config.js
+++ test/inscription_users/inscription_users_fonctionnal/cypress.config.js
@@ -1,9 +1,20 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin; // permet de lire le fichier en Gherkin
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin; // permet de faire la compilation des fichiers Gherkin
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    async setupNodeEvents(on, config) {
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+
+      on("file:preprocessor", bundler);
+      await addCucumberPreprocessorPlugin(on, config);
+
+      return config;
     },
+    specPattern: "cypress/e2e/features/*.feature"
   },
 });
