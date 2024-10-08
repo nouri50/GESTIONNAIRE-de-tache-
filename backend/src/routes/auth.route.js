3076695a6389f7fb699292b5e2ec145536fca1d9
nouri50 <n.morouche@gmail.com>
2024-10-08 23:40:09 +0200

    mise a jour profil

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import { forgotPassword, resetPassword, login, register } from '../controller/auth.controller.js';
-
+import { deleteOwnAccount } from '../controller/user.controller.js';
 const router = express.Router();
 
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password/:token', resetPassword);
 router.post('/login', login);
 router.post('/register', register);
+
+// Route pour que l'utilisateur connecté puisse supprimer son propre compte
+router.delete('/profile/delete', authMiddleware, deleteOwnAccount);
+
 
 export default router;
