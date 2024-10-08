c02300cc8a7627082914b901f8d0575e41db8d4a
nouri50 <n.morouche@gmail.com>
2024-10-08 23:40:09 +0200

    mise a jour profil

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -14,6 +14,27 @@ export const getAllUsers = async (req, res) => {
 };
 
 // Fonction pour supprimer un utilisateur
+// Fonction pour supprimer le compte de l'utilisateur connecté
+export const deleteOwnAccount = async (req, res) => {
+  try {
+    const userId = req.user.id; // Utilisateur connecté
+    const user = await User.findByPk(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: 'Utilisateur non trouvé' });
+    }
+
+    await user.destroy();
+    res.status(200).json({ message: 'Votre compte a été supprimé avec succès' });
+  } catch (error) {
+    console.error('Erreur lors de la suppression du compte :', error);
+    res.status(500).json({ message: 'Erreur lors de la suppression du compte' });
+  }
+};
+
+
+
+
 export const deleteUser = async (req, res) => {
   const userId = req.params.id;
   try {
