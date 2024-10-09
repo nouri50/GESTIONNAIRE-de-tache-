6c711f5ebe5302129e730ae1553f8716799a0cf4
Nesrine Zitouni <nesrinezitouni@MacBook-Air-de-Nesrine.local>
2024-10-10 00:35:10 +0200

    test automatisé suppression compte utilisateur avec un mot de passe incorrect

--- test/users_management/delete_users_incorrect_password/cypress/e2e/DeleteUsersIncorrectPassword.spec.cy.js
+++ test/users_management/delete_users_incorrect_password/cypress/e2e/DeleteUsersIncorrectPassword.spec.cy.js
@@ -1,5 +1,27 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Delete users incorrect password', () => {
+  it('I am on the website', () => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('I connect on the website', () => {
+    cy.xpath('//a[@href="/login"]').click()
+    cy.xpath('//input[@type="email"]').type('nzito055@icloud.com')
+    cy.xpath('//input[@type="password"]').type('nZit1103&994@')
+    cy.xpath('//button[@type="submit"]').click()
+    cy.wait(5000)
+  })
+
+  it('I redirected on the users management page', () => {
+    cy.get(':nth-child(4) > a').click()
+  })
+
+  it('I try delete an user account', () => {
+    cy.xpath('//button[@data-cy="delete-user-3"]').click({force: true})
+    cy.xpath('//input[@data-cy="password-input"]').type('testTest088@')
+    cy.xpath('//button[@data-cy="confirm-delete"]').click({force: true})
+  })
+
+  it('I receive an error message', () => {
+    cy.xpath('//').should('be.visible')
   })
 })
