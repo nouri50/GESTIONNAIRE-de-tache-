062e1414ec81d5faf74c62f7f7ba9df29fff3ee3
Nesrine Zitouni <nesrinezitouni@MacBook-Air-de-Nesrine.local>
2024-10-19 22:13:45 +0200

    test automatisé suppression compte utilisateur avec le role de user et le mot de passe valide

---
+++ test/users_management/delete_account_user_password_valide/cypress/support/step_definitions/DeleteAccountUserPasswordValideStepDefinitions.spec.cy.js
@@ -0,0 +1,31 @@
+import { Given, When, Then, And } from '@badeball/cypress-cucumber-preprocessor';
+require('cypress-xpath')
+
+Given('I am on the website', () => {
+    cy.visit('http://localhost:3000')
+})
+
+Given('I connect on the website', () => {
+    cy.xpath('//a[@href="/login"]').click()
+    cy.xpath('//input[@data-testid="login-email"]').type('test@test.test')
+    cy.xpath('//input[@data-testid="login-password"]').type('testTest001@')
+    cy.xpath('//button[@data-testid="login-submit"]').click()
+    cy.wait(5000)
+})
+
+When('I redirected on the users management page', () => {
+    cy.xpath('//a[@href="/gestion-utilisateur"]').click()
+})
+
+When('I click on the delete account button', () => {
+    cy.xpath('//img[@data-testid="delete-icon-user-3"]').click()
+})
+
+When('I fill the input with a valide password', () => {
+    cy.xpath('//input[@data-testid="admin-password-input"]').type('testTest01@')
+    cy.xpath('//button[@data-testid="confirm-delete-button"]').click()
+})
+
+Then('I receive an error message', () => {
+    cy.xpath('//p[@data-testid="error-message"]').should('be.visible')
+})
